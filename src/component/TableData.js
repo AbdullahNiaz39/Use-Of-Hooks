364b65c197b1e42ee64edcad04277e1999127317
Abdullah Niaz <abdullahniaz39@gmail.com>
2022-12-05 21:37:18 +0500

    using_one_UseState_addition

---
+++ src/component/TableData.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const TableData = ({ onData }) => {
+  //   const { onData } = props;
+  return (
+    <div>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          {onData.map((data, index) => (
+            <tr>
+              <th scope="row">{++index}</th>
+              <td>{data.name}</td>
+              <td>{data.age}</td>
+              <td>{data.desc}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TableData;

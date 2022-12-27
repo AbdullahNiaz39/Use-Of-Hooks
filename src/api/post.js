27fd6648f65bca03d1db4a3b71b629cc59be5c22
Abdullah Niaz <83068600+AbdullahNiaz39@users.noreply.github.com>
2022-12-27 01:51:36 -0800

    Update post.js

--- src/api/post.js
+++ src/api/post.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://localhost:3500",
 });
+
+
+/*///To run a json server ///
+ npx json-server -p 3500 -w src/component/table.json*/
